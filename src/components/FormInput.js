c9d671c09d28f7b3969e6d4f5afae8241c12f4f4
Kevin Pamaran <kevpamaran@gmail.com>
2020-10-04 18:38:45 -0700

    Implement ability to filter posts
    - Add FormInput component

---
+++ src/components/FormInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FormInput.css';
+
+function FormInput(props) {
+    return (
+        <div className="form-input">
+            <label className="form-input__label" htmlFor={props.id} >{ `${props.label}:` }</label>
+            <input className="form-input__field"
+                id={props.id}
+                type={props.type} 
+                value={props.value} 
+                onChange={e => props.onChange(e?.target?.value)}
+            />
+        </div>
+    )
+}
+
+FormInput.defaultProps = {
+    type: 'text'
+}
+
+FormInput.propTypes = {
+    type: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+}
+
+export default FormInput;
--- src/views/PostsList.js
+++ src/views/PostsList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import SimplePost from '../components/SimplePost'
+import FormInput from '../components/FormInput'
 
 // Renders a list of each post's title and thumbnail
 function PostsList({ posts, subreddit }) {
+
+    const [filterStr, setFilter] = useState('');
+
+    if (filterStr) {
+        posts = posts.filter(post => {
+            const title = post.title.toLowerCase();
+            return title.includes(filterStr.toLowerCase());
+        })
+    }
+
     return (
         <div>
             <h1>{ `/r/${subreddit} Browser` }</h1>
+            <FormInput
+                id="post-filter"
+                label="Filter"
+                value={filterStr}
+                onChange={setFilter}
+            />
             <ul>
                 {posts.map(post => (
                     <SimplePost 
